1645e48ef13ab9ab4d99298ef410df82a498f3e4
Official-Krish <krishanand0103@gmail.com>
2025-09-22 17:20:12 +0530

    [feat]: added routes for swap and balances

--- backend/src/routes/solana.rs
+++ backend/src/routes/solana.rs
@@ -1,5 +1,9 @@
+use std::{sync::{Arc, Mutex}, vec};
+
 use actix_web::{web, HttpResponse, Result};
 use serde::{Deserialize, Serialize};
+use store::Store;
+use solana_sdk::{pubkey::Pubkey, transaction::Transaction};
 
 #[derive(Deserialize)]
 pub struct QuoteRequest {
@@ -66,18 +70,53 @@ pub struct SwapInfo {
 
 #[derive(Deserialize)]
 pub struct SwapRequest {
+    to: String,
+    amount: f64,
+    user_id: String,
 }
 
 #[derive(Serialize)]
 pub struct SwapResponse {
+    pub signature: Transaction,
 }
 
 #[derive(Serialize)]
 pub struct BalanceResponse {
+    pub lamports: u64,
 }
 
 #[derive(Serialize)]
 pub struct TokenBalanceResponse {
+    pub amount: u64,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GetKeyPairInput {
+    pub user_id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AggAndStep1Output {
+    pub agg_message1: String,
+    pub secret_agg_step_one: String,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct AggAndStep2Input {
+    pub keypair_base64: String,
+    pub amount: f64,
+    pub to: String,
+    pub keys: Vec<String>,
+    pub first_messages: Vec<String>,
+    pub secret_state: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct SignatureAggregationInput {
+    pub amount: f64,
+    pub to: Pubkey,
+    pub keys: Vec<Pubkey>,
+    pub signatures: Vec<String>,
 }
 
 #[actix_web::post("/quote")]
@@ -122,27 +161,181 @@ pub async fn quote(req: web::Json<QuoteRequest>) -> Result<HttpResponse> {
 
 #[actix_web::post("/swap")]
 pub async fn swap(req: web::Json<SwapRequest>) -> Result<HttpResponse> {
-    
-    let response = SwapResponse {};
-    
-    Ok(HttpResponse::Ok().json(response))
+    let mut keypairs = vec![];
+
+    let client = reqwest::Client::new();
+    let target_url = vec![
+        "http://localhost:9000/getKeyPair",
+        "http://localhost:9001/getKeyPair",
+    ];
+
+    for url in target_url {
+        match client.post(url)
+            .json(&GetKeyPairInput {
+                user_id: req.user_id.clone(),
+            })
+            .send()
+            .await
+        {
+            Ok(response) => {
+                if response.status().is_success() {
+                    let response_body = match response.text().await {
+                        Ok(body) => body,
+                        Err(_) => {
+                            let error_message = format!("Failed to read response body from {}", url);
+                            return Ok(HttpResponse::InternalServerError().body(error_message));
+                        }
+                    };
+                    keypairs.push(response_body);
+                } else {
+                    let error_message = format!("Failed to send data to {}: {:?}", url, response.status());
+                    return Ok(HttpResponse::InternalServerError().body(error_message));
+                }
+            }
+            Err(e) => {
+                let error_message = format!("Error sending request to {}: {:?}", url, e);
+                return Ok(HttpResponse::InternalServerError().body(error_message));
+            }
+        }
+    }
+
+    let mut step1_response = vec![vec![]; keypairs.len()];
+
+    for (i, keypair) in keypairs.iter().enumerate() {
+        match client.post("http://localhost:8080/agg-send-step1")
+            .json(&keypair)
+            .send()
+            .await
+        {
+            Ok(response) => {
+                if response.status().is_success() {
+                    let response_body = match response.json::<AggAndStep1Output>().await {
+                        Ok(body) => body,
+                        Err(_) => {
+                            let error_message = format!("Failed to parse JSON from agg-send-step1");
+                            return Ok(HttpResponse::InternalServerError().body(error_message));
+                        }
+                    };
+                    step1_response[i] = vec![
+                        response_body.agg_message1,
+                        response_body.secret_agg_step_one,
+                    ];
+                } else {
+                    let error_message = format!("Failed to send data to agg-send-step1: {:?}", response.status());
+                    return Ok(HttpResponse::InternalServerError().body(error_message));
+                }
+            }
+            Err(e) => {
+                let error_message = format!("Error sending request to agg-send-step1: {:?}", e);
+                return Ok(HttpResponse::InternalServerError().body(error_message));
+            }
+        };
+    }
+
+    let mut step2_response = vec![];
+    for i in 0..keypairs.len() {
+        match client.post("http://localhost:8080/agg-send-step2")
+            .json(&AggAndStep2Input{
+                keypair_base64: keypairs[i].clone(),
+                amount: req.amount,
+                to: req.to.clone(),
+                keys: keypairs.clone(),
+                first_messages: step1_response.iter().map(|r| r[0].clone()).collect(),
+                secret_state: step1_response[i][1].clone(),
+            })
+            .send()
+            .await
+        {
+            Ok(response) => {
+                if response.status().is_success() {
+                    let response_body = match response.text().await {
+                        Ok(body) => body,
+                        Err(_) => {
+                            let error_message = format!("Failed to read response body from agg-send-step2");
+                            return Ok(HttpResponse::InternalServerError().body(error_message));
+                        }
+                    };
+                    step2_response.push(response_body);
+                } else {
+                    let error_message = format!("Failed to send data to agg-send-step2: {:?}", response.status());
+                    return Ok(HttpResponse::InternalServerError().body(error_message));
+                }
+            }
+            Err(e) => {
+                let error_message = format!("Error sending request to agg-send-step2: {:?}", e);
+                return Ok(HttpResponse::InternalServerError().body(error_message));
+            }
+        };
+    }
+
+    match client.post("http://localhost:8080/aggregate-signatures-broadcast")
+        .json(&SignatureAggregationInput{
+            amount: req.amount as f64,
+            to: req.to.parse().unwrap(),
+            keys: keypairs.iter().map(|k| k.parse().unwrap()).collect(),
+            signatures: step2_response.iter().map(|s| s.parse().unwrap()).collect(),
+        })
+        .send()
+        .await
+    {
+        Ok(response) => {
+            if response.status().is_success() {
+                let response_body = match response.json::<Transaction>().await {
+                    Ok(body) => body,
+                    Err(_) => {
+                        let error_message = format!("Failed to read response body from aggregate-signatures-broadcast");
+                        return Ok(HttpResponse::InternalServerError().body(error_message));
+                    }
+                };
+                return Ok(HttpResponse::Ok().json(SwapResponse {
+                    signature: response_body,
+                }));
+            } else {
+                let error_message = format!("Failed to send data to aggregate-signatures-broadcast: {:?}", response.status());
+                return Ok(HttpResponse::InternalServerError().body(error_message));
+            }
+        }
+        Err(e) => {
+            let error_message = format!("Error sending request to aggregate-signatures-broadcast: {:?}", e);
+            return Ok(HttpResponse::InternalServerError().body(error_message));
+        }
+    };
 }
 
 #[actix_web::get("/sol-balance/{pubkey}")]
-pub async fn sol_balance() -> Result<HttpResponse> {
-    
+pub async fn sol_balance(path: web::Path<String>, store: web::Data<Arc<Mutex<Store>>>) -> Result<HttpResponse> {
+    let pubkey = path.into_inner();
+    let locked_store = match store.lock() {
+        Ok(locked) => locked,
+        Err(_) => return Ok(HttpResponse::InternalServerError().body("Failed to lock store")),
+    };
+    let balance = match locked_store.get_sol_balance(pubkey.to_string()).await {
+        Ok(user) => user,
+        Err(err) => return Ok(HttpResponse::BadRequest().body(err.to_string())),
+    };
+
     let response = BalanceResponse {
+        lamports: balance,
     };
     
     Ok(HttpResponse::Ok().json(response))
 }
 
 #[actix_web::get("/token-balance/{pubkey}/{mint}")]
-pub async fn token_balance() -> Result<HttpResponse> {    
-    
-    let response = TokenBalanceResponse {
-        
+pub async fn token_balance(path: web::Path<(String, String)>, store: web::Data<Arc<Mutex<Store>>>) -> Result<HttpResponse> {
+    let (pubkey, mint) = path.into_inner();
+    let locked_store = match store.lock() {
+        Ok(locked) => locked,
+        Err(_) => return Ok(HttpResponse::InternalServerError().body("Failed to lock store")),
     };
-    
+    let balance = match locked_store.get_token_balance(pubkey.to_string(), mint.to_string()).await {
+        Ok(user) => user,
+        Err(err) => return Ok(HttpResponse::BadRequest().body(err.to_string())),
+    };
+
+    let response = TokenBalanceResponse {
+        amount: balance,
+    };
+
     Ok(HttpResponse::Ok().json(response))
-}
+}   
